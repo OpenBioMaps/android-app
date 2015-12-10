c80bd8a424f7a4e83ab9323a3966e977bdcee93c
gerybravo <petergergo2.0@gmail.com>
2015-12-10 13:31:05 +0100

    Image files upload works properly.

--- OpenBioMapsApp/app/src/main/java/hu/ektf/iot/openbiomapsapp/upload/FileMapCreator.java
+++ OpenBioMapsApp/app/src/main/java/hu/ektf/iot/openbiomapsapp/upload/FileMapCreator.java
@@ -8,6 +8,7 @@ import java.util.List;
 import java.util.Map;
 
 import retrofit.mime.TypedFile;
+import timber.log.Timber;
 
 /**
  * Created by szugyi on 20/11/15.
@@ -16,14 +17,17 @@ public class FileMapCreator {
     public static Map<String, TypedFile> createFileMap(List<String>... fileLists){
         Map<String, TypedFile> fileMap = new HashMap<String, TypedFile>();
 
+        int count = 0;
         for(int i = 0; i < fileLists.length; i++) {
             List<String> files = fileLists[i];
             for (int j = 0; j < files.size(); j++) {
                 String filePath = files.get(j);
                 String mimeType = getMimeType(filePath);
+                Timber.v("" + mimeType);
                 File file = new File(filePath);
                 TypedFile typedImage = new TypedFile(mimeType, file);
-                fileMap.put(String.format(BioMapsServiceInterface.PARAM_FILE_ARRAY_FORMAT, i), typedImage);
+                fileMap.put(String.format(BioMapsServiceInterface.PARAM_FILE_ARRAY_FORMAT, count), typedImage);
+                count++;
             }
         }
 
